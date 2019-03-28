510d6d5815f344b2d6705316ecf64a2007f81ea5
Eamonn <efbdickson@gmail.com>
2019-03-28 11:11:07 +1100

    Init

---
+++ MovieStore/MovieStore/Data/MovieStoreApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieStore.Data
+{
+    public class MovieStoreApplicationDbContext : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            option.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=aspnet-MovieStore-FEF59AEB-10DA-4837-8377-7113ED9537CC;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
