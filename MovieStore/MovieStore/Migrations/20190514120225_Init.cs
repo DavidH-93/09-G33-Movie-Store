998e0c6672b808f8693a70ad90bb284339653892
David Hepburn <12972028@student.uts.edu.au>
2019-05-14 22:24:33 +1000

    Whaever

---
+++ MovieStore/MovieStore/Migrations/20190514120225_Init.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MovieStore.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Published",
+                table: "Movie",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Published",
+                table: "Movie");
+        }
+    }
+}
