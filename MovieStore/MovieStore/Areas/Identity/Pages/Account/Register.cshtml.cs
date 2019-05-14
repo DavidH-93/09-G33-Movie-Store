857b94aa2584079eda947fad8c56ba0b6c4f29e8
Sushan KC <12839256@student.uts.edu.au>
2019-05-14 21:16:18 +1000

    Added fields to Register Page

--- MovieStore/MovieStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MovieStore/MovieStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -40,9 +40,31 @@ namespace MovieStore.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+            [StringLength(30)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+            [StringLength(30)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
+            [StringLength(30)]
+            [Display(Name = "Username")]
+            public string Username { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
+
+            [Required]
+            [Phone]
+            [Display(Name = "Phone")]
+            public string Phone { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
