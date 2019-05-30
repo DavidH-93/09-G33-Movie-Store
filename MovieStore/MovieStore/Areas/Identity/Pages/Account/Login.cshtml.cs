ab6105c70416aeb346756fe674d3eb7cd0b34f73
Eamonn <efbdickson@gmail.com>
2019-05-31 07:00:38 +1000

    minor registration tweaks

--- MovieStore/MovieStore/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MovieStore/MovieStore/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -83,7 +83,7 @@ namespace MovieStore.Areas.Identity.Pages.Account
 
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: true);
+                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     
